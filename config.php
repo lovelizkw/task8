db38aa615934e38ea1ec2bb1b3250157a3db98c8
lovelizkw <komissarenkoliza2006@mail.ru>
2026-05-27 00:32:34 +0400

    Update config.php

--- config.php
+++ config.php
@@ -10,6 +10,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    die("Ошибка подключения к БД: " . $e->getMessage());
+    die("Ошибка подключения: " . $e->getMessage());
 }
 ?>
